0c51923fa0ec61a037decd56c69306b63a8edc8e
Alexander Santos <alexsantosantana@live.com>
2025-05-27 22:47:10 -0400

    feat: add @sveltia/cms integration and update configuration files

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		const mod = await import('@sveltia/cms');
+
+		mod.init();
+	});
+</script>
--- src/types/datocms-client.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'datocms-client' {
-	const content: any;
-	const SiteClient: any;
-	export { SiteClient, content };
-}
